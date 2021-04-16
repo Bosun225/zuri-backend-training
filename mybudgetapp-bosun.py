ff8cc9471637a6a0d573cb5b9b7425e42f434b56
codex_Que <45357287+Bosun225@users.noreply.github.com>
2021-04-16 16:33:59 +0100

    not yet ready

---
+++ mybudgetapp-bosun.py
@@ -0,0 +1,51 @@
+class budget:
+    """created the budget class"""
+    def __init__(self, account, deposit, withdraw, balance, transfer):
+        self.account = account
+        self.deposit = deposit
+        self.balance = balance
+        self.withdraw = withdraw        
+        self.transfer = transfer
+
+    
+    def deposit(self):
+        account = []
+        print("How much do you want to deposit")
+        amt = input(" \n >>>")
+        amt.append(account)
+
+    def balance(self):
+        print(f'Your current balance is {amt}')
+
+    def withdraw(self):
+        print('How much would you like to withdraw')
+        withdraw_amt = input("\n >>>") 
+
+        if withdraw_amt < amt:
+            new_balance = amt - withdraw_amt
+            print(f'Your new balance is {new_balance} ')
+
+        else:
+            print("You have entered an invalid amount")
+
+    def transfer(self):
+        
+        
+
+        
+
+
+
+
+
+
+
+    
+# print("This is a class of a car")
+
+# # Car()
+# # print(type(Car))
+
+# car_1 = Car()
+# print(type(car_1))
+
